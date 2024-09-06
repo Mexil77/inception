4e08fc178a83003b07e73eff223e0372c7943cd2
Emmanuel Garcia torres <emgarcia@c1r17s2.42madrid.com>
2024-09-06 12:49:29 +0200

    change hosts

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ up:
 down:
 	docker compose down
 fclean: down
-	# docker volume rm $(shell docker volume ls -q)
+	docker volume rm $(shell docker volume ls -q)
 	docker rmi -f $(shell docker images -qa)
 	docker system prune -a
 re: fclean all
--- src/wordpress/script.sh
+++ src/wordpress/script.sh
@@ -12,7 +12,7 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url="localhost" --title="$WP_TITLE" --admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+./wp-cli.phar core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 # ./wp-cli.phar user create "$WP_USER_NAME" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --allow-root
 
 ./wp-cli.phar theme install rainfall --activate --allow-root
